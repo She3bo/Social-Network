9a8705a0d0cfa9bddc0332a292095bd51a5345fc
AhmedShaban <ahmedshaban33ah@gmail.com>
2019-12-24 01:04:28 +0200

    add new functions

--- Social Network/src/social/network/SocialNetwork.java
+++ Social Network/src/social/network/SocialNetwork.java
@@ -21,11 +21,31 @@ public class SocialNetwork {
             } else if (c == 2) {
                 System.out.print("Email : ");
                 String email = sc.next();
-                System.out.print("\nPassword : ");
+                System.out.print("Password : ");
                 String password = sc.next();
-                user.login(email, password);
-            } else {
+                if (user.login(email, password)) {
+                    // search for user to add to his frind list if he is not frind 
+                    System.out.println(" 1 - Search for user \n 2 - show frind requests \n 3 - upgrade to Premium subscription ");
+                    c = sc.nextInt();
+                    if (c == 1) {
+                       System.out.print("Enater User Name : ");
+                       String name = sc.next();
+                       //user.findFriends(name);
+                    } else if (c == 2) {
+                        System.out.println(user.friendRequests);
+                    } else if (c == 3) {
+
+                    } else {
+                        System.out.println("Whoops! you should enter 1 or 2 or 3");
+                    }
+                } else {
+                    System.out.println("Sorry! Your email or password isn't coorect. try again");
+                }
+
+            } else if (c == 3) {
                 return;
+            } else {
+                System.out.println("Whoops! you should enter 1 or 2 or 3");
             }
         }
 
